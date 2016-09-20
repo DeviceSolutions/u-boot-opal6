138ac2004ef3424c47c13e8761057328df6fc781
John Inglis <john@device.solutions>
2016-09-21 02:25:28 +1200

    Add rootwait to default env to cater for delayed mmc startup

--- include/configs/dsopal6.h
+++ include/configs/dsopal6.h
@@ -213,8 +213,7 @@
         "hdmi=setenv video video=mxcfb0:dev=hdmi,1920x1080M@60,if=RGB24  video=mxcfb1:off video=mxcfb2:off video=mxcfb3:off ldb=sep0\0" \
         "video=mxcfb0:dev=ldb,LDB-XGA,if=RGB666 video=mxcfb1:dev=hdmi,1920x1080M@60,if=RGB24 video=mxcfb2:off video=mxcfb3:off ldb=sep0\0" \
         "rootfs=/dev/mmcblk3p3 rw\0" \
-        "mmcargs=setenv bootargs enable_wait_mode=off ${video} console=ttymxc3,115200 consoleblank=0 vmalloc=400M fbmem=28M root=${rootfs}\0" \
-        "modem_up=gpio set 170; gpio set 174; gpio set 107; gpio set 106;\0"
+        "mmcargs=setenv bootargs enable_wait_mode=off ${video} console=ttymxc3,115200 consoleblank=0 vmalloc=400M fbmem=28M root=${rootfs} rootwait\0"
 
         /*"update_uboot_from_emmc=" \
             "if fatload mmc 1 ${loadaddr} u-boot.imx; then " \
